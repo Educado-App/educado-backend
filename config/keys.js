f5ea58b154813486f87824495cf8afa9118e1da0
MortenMunk <mortenmunk97@gmail.com>
2023-09-19 12:16:32 +0200

    updated keys

--- config/keys.js
+++ config/keys.js
@@ -2,9 +2,9 @@ const dotenv = require("dotenv");
 
 // Load environment variables based on NODE_ENV
 if (process.env.NODE_ENV === "production") {
-  dotenv.config({ path: ".env.prod" });
+  dotenv.config({ path: "./config/.env.prod" });
 } else {
-  dotenv.config({ path: ".env" });
+  dotenv.config({ path: "./config/.env" });
 }
 
 // Access the environment variables
