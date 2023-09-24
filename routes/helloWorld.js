07d699791db9faf0dbace3715f50b8c08e87daf0
Jatewo <jacobterpewoods@gmail.com>
2023-09-24 18:42:08 +0200

    Removed test route

--- routes/helloWorld.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const { makeExpressCallback } = require('../helpers/express')
-
-router.get("/helloworld", async (req, res) => {
-    res.send("Hello World!")
-});
-    
-module.exports = router;
