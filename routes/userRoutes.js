ec1494d8d7b620caa72c0372c63d8dab36596c4c
Mafusn <Magnus.peetz.holt@gmail.com>
2023-10-03 11:20:12 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,11 +8,8 @@ const requireLogin = require('../middlewares/requireLogin');
 
 router.delete("/delete/:id", requireLogin, async (req, res) => {
   try {
-    // Get the authenticated user's ID from req.user.id
     const { id } = req.params;
 
-    // Use Mongoose to find and delete the user by ID
-    console.log("Deleting user with ID:", id)
     await User.findByIdAndDelete(id);
 
 		// Send a success response
@@ -27,13 +24,9 @@ router.delete("/delete/:id", requireLogin, async (req, res) => {
 // Update User Email route
 router.put("/update-email/:id", requireLogin, async (req, res) => {
   try {
-    // Get the authenticated user's ID from req.user.id
     const { id } = req.params;
-
-    // Get the new email from the request body
     const { newEmail } = req.body;
 
-    // Use Mongoose to find the user by ID and update the email
     const updatedUser = await User.findByIdAndUpdate(
       id,
       { email: newEmail },
@@ -41,14 +34,11 @@ router.put("/update-email/:id", requireLogin, async (req, res) => {
     );
 
     if (!updatedUser) {
-      // User with the specified ID was not found
       return res.status(404).json({ error: "User not found" });
     }
 
-    // Send the updated user data as a response
     res.status(200).json({ message: "Email updated successfully", user: updatedUser });
   } catch (error) {
-    // Handle any errors and send an error response
     console.error("Error updating email:", error);
     res.status(500).json({ error: "An error occurred while updating the email" });
   }
@@ -57,13 +47,9 @@ router.put("/update-email/:id", requireLogin, async (req, res) => {
 // Update User first name route
 router.put("/update-first_name/:id", requireLogin, async (req, res) => {
   try {
-    // Get the authenticated user's ID from req.user.id
     const { id } = req.params;
-
-    // Get the new name from the request body
     const { newFirstName } = req.body;
 
-    // Use Mongoose to find the user by ID and update the name
     const updatedUser = await User.findByIdAndUpdate(
       id,
       { firstName: newFirstName },
@@ -71,14 +57,11 @@ router.put("/update-first_name/:id", requireLogin, async (req, res) => {
     );
 
     if (!updatedUser) {
-      // User with the specified ID was not found
       return res.status(404).json({ error: "User not found" });
     }
 
-    // Send the updated user data as a response
     res.status(200).json({ message: "First name updated successfully", user: updatedUser });
   } catch (error) {
-    // Handle any errors and send an error response
     console.error("Error updating first name:", error);
     res.status(500).json({ error: "An error occurred while updating the first name" });
   }
@@ -87,13 +70,9 @@ router.put("/update-first_name/:id", requireLogin, async (req, res) => {
 // Update User last name route
 router.put("/update-last_name/:id", requireLogin, async (req, res) => {
   try {
-    // Get the authenticated user's ID from req.user.id
     const { id } = req.params;
-
-    // Get the new name from the request body
     const { newLastName } = req.body;
 
-    // Use Mongoose to find the user by ID and update the name
     const updatedUser = await User.findByIdAndUpdate(
       id,
       { lastName: newLastName },
@@ -101,14 +80,11 @@ router.put("/update-last_name/:id", requireLogin, async (req, res) => {
     );
 
     if (!updatedUser) {
-      // User with the specified ID was not found
       return res.status(404).json({ error: "User not found" });
     }
 
-    // Send the updated user data as a response
     res.status(200).json({ message: "Last name updated successfully", user: updatedUser });
   } catch (error) {
-    // Handle any errors and send an error response
     console.error("Error updating last name:", error);
     res.status(500).json({ error: "An error occurred while updating the last name" });
   }
