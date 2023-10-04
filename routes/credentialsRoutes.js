b211f588b84226614990c03ff713754b9599937e
MGLarsenSW <mathias.larsen9@gmail.com>
2023-10-04 13:37:12 +0200

    updated bcrypt to match helpers file

--- routes/credentialsRoutes.js
+++ routes/credentialsRoutes.js
@@ -3,47 +3,33 @@ const { ContentCreatorApplication } = require("../models/ContentCreatorApplicati
 const bcrypt = require('bcrypt');
 
 const jwt = require("jsonwebtoken");
-
-const saltRounds = 5;
+const { compare, encrypt } = require("../helpers/password");
 
 // Content Creator Application Route
 router.post("/signup", async (req, res) => {
-  bcrypt.hash(req.body.password, saltRounds)
-  .then(hash => {
-    req.body.password = hash;
-    const form = req.body;
+  const hashpassword = encrypt(req.body.password)
+    
+  const form = req.body;
+  form.password = hashpassword;
 
-    const doc = ContentCreatorApplication(form);
-    const created = doc.save();
+  const doc = ContentCreatorApplication(form);
+  const created = doc.save();
 
 
-    try {
-      res.status(201);
-      res.send(created);
-      console.log(req.body.password)
-      
-    } catch (error) {
-      res.status(400);
-      res.send(error.message);
-    }
-})
-
-  
-
-  // Validate form ...
-  
+  try {
+    res.status(201);
+    res.send(created);
+    
+  } catch (error) {
+    res.status(400);
+    res.send(error.message);
+  }
 });
 
 router.post("/login", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const contentCreator = await ContentCreatorApplication.findOne({ email: email })
-
-  /*bcrypt.compare("hejsa", bcrypt.hashSync(contentCreator.password, saltRounds), function(result) {
-    const password = req.body.password;
-    console.log(password)
-    console.log(contentCreator.password)
-  });*/
   
   try {
      
@@ -55,7 +41,7 @@ router.post("/login", async (req, res) => {
       throw error;
     }
     
-    if (!bcrypt.compare(password, contentCreator.password)){
+    if (!compare(password, contentCreator.password)){
       console.log("Wrong Password")
       const error = new Error("Wrong password");
       res.status(401);
@@ -63,19 +49,18 @@ router.post("/login", async (req, res) => {
       throw error;
     }
     
-
-    if (bcrypt.compare(password, contentCreator.password) && email == contentCreator.email){
+    if (compare(password, contentCreator.password) && email == contentCreator.email){
       const token = jwt.sign({email: contentCreator.email, password: contentCreator.password},'secretfortoken',{ expiresIn: '3h' });
 
       res.status(200).json({ token: token, email: contentCreator.email, password: contentCreator.password, name: contentCreator.name});
       console.log("Successful Login")
     }
+
   } catch (err) {
     if(!err.statusCode) {
       err.statusCode = 500;
     }
   }
-
 });
 
 module.exports = router;
