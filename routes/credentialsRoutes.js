081492324ca719f5fc68a59d7dcb7b9ca1a46ecb
Benjamin <benjaminspeyerlarsen@gmail.com>
2023-10-04 12:09:58 +0200

    Password Hashing
    Passwords are hashed, and able to be used to authenticate login

--- routes/credentialsRoutes.js
+++ routes/credentialsRoutes.js
@@ -4,9 +4,11 @@ const bcrypt = require('bcrypt');
 
 const jwt = require("jsonwebtoken");
 
+const saltRounds = 5;
+
 // Content Creator Application Route
 router.post("/signup", async (req, res) => {
-  bcrypt.hash(req.body.password, 5)
+  bcrypt.hash(req.body.password, saltRounds)
   .then(hash => {
     req.body.password = hash;
     const form = req.body;
@@ -14,17 +16,18 @@ router.post("/signup", async (req, res) => {
     const doc = ContentCreatorApplication(form);
     const created = doc.save();
 
-    res.status(201);
-    res.send(created);
-  })
 
-  try {
-    console.log(req.body.password)
-    
-  } catch (error) {
-    res.status(400);
-    res.send(error.message);
-  }
+    try {
+      res.status(201);
+      res.send(created);
+      console.log(req.body.password)
+      
+    } catch (error) {
+      res.status(400);
+      res.send(error.message);
+    }
+})
+
   
 
   // Validate form ...
@@ -34,10 +37,16 @@ router.post("/signup", async (req, res) => {
 router.post("/login", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+  const contentCreator = await ContentCreatorApplication.findOne({ email: email })
+
+  /*bcrypt.compare("hejsa", bcrypt.hashSync(contentCreator.password, saltRounds), function(result) {
+    const password = req.body.password;
+    console.log(password)
+    console.log(contentCreator.password)
+  });*/
   
   try {
      
-    const contentCreator = await ContentCreatorApplication.findOne({ email: email })
     if(!contentCreator){
       console.log("Wrong User")
       const error = new Error("An account with this email does not exist!");
@@ -46,17 +55,16 @@ router.post("/login", async (req, res) => {
       throw error;
     }
     
-    if (password !== contentCreator.password){
+    if (!bcrypt.compare(password, contentCreator.password)){
       console.log("Wrong Password")
       const error = new Error("Wrong password");
       res.status(401);
       error.statusCode = 401;
       throw error;
-      
     }
     
 
-    if (password == contentCreator.password && email == contentCreator.email){
+    if (bcrypt.compare(password, contentCreator.password) && email == contentCreator.email){
       const token = jwt.sign({email: contentCreator.email, password: contentCreator.password},'secretfortoken',{ expiresIn: '3h' });
 
       res.status(200).json({ token: token, email: contentCreator.email, password: contentCreator.password, name: contentCreator.name});
