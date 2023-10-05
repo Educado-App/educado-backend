1501feeaf8788d0eb658264074c6bfd5b7ea77e7
Jatewo <jacobterpewoods@gmail.com>
2023-10-05 13:11:54 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router()
-const passport = require("passport"); // Import passport library module
 const { User } = require("../models/User"); // Import User model
 const bcrypt = require("bcrypt"); // Import bcrypt library module
 const jwt = require("jsonwebtoken"); // Import jsonwebtoken library module
