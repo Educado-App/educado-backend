90d3f8c7c19b8bc692a075fab10f99bc78c5cece
Jatewo <jacobterpewoods@gmail.com>
2023-10-05 13:15:02 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,5 @@
 const router = require('express').Router()
 const { User } = require("../models/User"); // Import User model
-const bcrypt = require("bcrypt"); // Import bcrypt library module
-const jwt = require("jsonwebtoken"); // Import jsonwebtoken library module
-const keys = require("../config/keys"); // Import keys from config/keys.js
 
 const { makeExpressCallback } = require('../helpers/express')
 const { authEndpointHandler } = require('../auth');
