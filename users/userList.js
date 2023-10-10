640741d6ef40ce794cc1d631339b45dbe26a237e
Mafusn <Magnus.peetz.holt@gmail.com>
2023-10-10 16:07:52 +0200

    Update userList.js

--- users/userList.js
+++ users/userList.js
@@ -14,7 +14,7 @@ module.exports = function makeUserList(db_model) {
         return await db_model.create(user);
     }
 
-    async function remove(user = {}) {
+    async function remove(user) {
         const results = await db_model.deleteMany(user);
         return results.deletedCount;
     }
