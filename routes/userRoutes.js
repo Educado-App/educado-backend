ab95d1578137c743a8ad8a6e4ff2b5bfda995df4
sofiiagran <sofiiagran@gmail.com>
2023-10-12 13:56:02 +0200

    removed idea and fixed 404 to 204

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -100,7 +100,7 @@ router.get('/:id/subscriptions', async (req, res) => {
     const user = await User.findById(userId).select('subscriptions');
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(204);
     }
 
     const subscribedCourses = user.subscriptions;
