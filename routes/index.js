4f0a2e7ba78f4e1e834e316fe4e0ef103c178212
BenjaminVilladsen <benmicvil@gmail.com>
2023-10-12 10:07:04 +0200

    okay

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,27 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
 // Routes
-const CourseRoutes = require('./courseRoutes')
-const AWSRoutes = require('./bucketRoutes')
-const AuthRoutes = require('./authRoutes')
-const SignupRoutes = require('./signupRoutes')
-const ApplicationRoutes = require('./applicationRoutes')
-const MailRoutes = require('./mailRoutes')
-const UserRoutes = require('./userRoutes')
-const bucketRoutesGCP = require('./bucketRoutesGCP')
+const CourseRoutes = require("./courseRoutes");
+const AWSRoutes = require("./bucketRoutes");
+const AuthRoutes = require("./authRoutes");
+const SignupRoutes = require("./signupRoutes");
+const ApplicationRoutes = require("./applicationRoutes");
+const MailRoutes = require("./mailRoutes");
+const UserRoutes = require("./userRoutes");
+const bucketRoutesGCP = require("./bucketRoutesGCP");
 
 // Print all routes defined in app
-router.get('/api', (req, res) => {
-    res.send(router.stack)
-})
+router.get("/api", (req, res) => {
+  res.send(router.stack);
+});
 
-router.use('/api', CourseRoutes)
-router.use('', AWSRoutes)
-router.use('/api/content', bucketRoutesGCP)
-router.use('/api', AuthRoutes)
-router.use('/api/signup', SignupRoutes)
-router.use('/api/applications', ApplicationRoutes)
-router.use('/api/mail',MailRoutes)
-router.use('/api/user', UserRoutes)
+router.use("/api", CourseRoutes);
+router.use("", AWSRoutes);
+router.use("/api/content", bucketRoutesGCP);
+router.use("/api", AuthRoutes);
+router.use("/api/signup", SignupRoutes);
+router.use("/api/applications", ApplicationRoutes);
+router.use("/api/mail", MailRoutes);
+router.use("/api/user", UserRoutes);
 
-module.exports = router
+module.exports = router;
