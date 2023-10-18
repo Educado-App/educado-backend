22aae60099505cd03dd96f68c67d59bed0510b75
Andazu <andersmazen@gmail.com>
2023-10-18 14:00:56 +0200

    Update exerciseRoutes.js

--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -1,23 +1,31 @@
-const router = require('express').Router();
-const errorCodes = require('../helpers/errorCodes');
+const router = require("express").Router();
+const errorCodes = require("../helpers/errorCodes");
 const adminOnly = require("../middlewares/adminOnly");
 
 // Models
-const { CourseModel } = require('../models/Courses');
-const { SectionModel } = require('../models/Sections');
-const { ComponentModel } = require('../models/Components');
-const { ExerciseModel } = require('../models/Exercises');
-const { UserModel } = require('../models/Users');
+const { CourseModel } = require("../models/Courses");
+const { SectionModel } = require("../models/Sections");
+const { ComponentModel } = require("../models/Components");
+const { ExerciseModel } = require("../models/Exercises");
+const { UserModel } = require("../models/Users");
 const {
-	ContentCreatorApplication,
+  ContentCreatorApplication,
 } = require("../models/ContentCreatorApplication");
 const requireLogin = require("../middlewares/requireLogin");
 const { IdentityStore } = require("aws-sdk");
 
 // Get all exercises
-router.get('/', async (req, res) => {
-	const list = await ExerciseModel.find();
-	res.send(list);
+router.get("/", async (req, res) => {
+  const list = await ExerciseModel.find();
+  res.send(list);
+});
+
+// Get specific exercise
+router.get("/:exercise_id", async (req, res) => {
+  const { exercise_id } = req.params;
+  const exerciseObjectId = mongoose.Types.ObjectId(exercise_id);
+  const exercise = await ExerciseModel.findById(exerciseObjectId);
+  res.send(exercise);
 });
 
 module.exports = router;
