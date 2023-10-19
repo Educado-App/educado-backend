3216b3780f1ece1999da191e10f2258f8356d4b6
Mafusn <Magnus.peetz.holt@gmail.com>
2023-10-19 12:14:31 +0200

    update linting

--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -1,28 +1,28 @@
-const router = require("express").Router();
-const errorCodes = require("../helpers/errorCodes");
-const adminOnly = require("../middlewares/adminOnly");
-const mongoose = require("mongoose");
+const router = require('express').Router();
+const errorCodes = require('../helpers/errorCodes');
+const adminOnly = require('../middlewares/adminOnly');
+const mongoose = require('mongoose');
 
 // Models
-const { CourseModel } = require("../models/Courses");
-const { SectionModel } = require("../models/Sections");
-const { ComponentModel } = require("../models/Components");
-const { ExerciseModel } = require("../models/Exercises");
-const { UserModel } = require("../models/Users");
+const { CourseModel } = require('../models/Courses');
+const { SectionModel } = require('../models/Sections');
+const { ComponentModel } = require('../models/Components');
+const { ExerciseModel } = require('../models/Exercises');
+const { UserModel } = require('../models/Users');
 const {
   ContentCreatorApplication,
-} = require("../models/ContentCreatorApplication");
-const requireLogin = require("../middlewares/requireLogin");
-const { IdentityStore } = require("aws-sdk");
+} = require('../models/ContentCreatorApplication');
+const requireLogin = require('../middlewares/requireLogin');
+const { IdentityStore } = require('aws-sdk');
 
 // Get all exercises
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const list = await ExerciseModel.find();
   res.send(list);
 });
 
 // Get specific exercise
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const exercise = await ExerciseModel.findById(id);
   res.send(exercise);
--- routes/sectionRoutes.js
+++ routes/sectionRoutes.js
@@ -1,27 +1,27 @@
-const router = require("express").Router();
-const errorCodes = require("../helpers/errorCodes");
-const adminOnly = require("../middlewares/adminOnly");
-const mongoose = require("mongoose");
+const router = require('express').Router();
+const errorCodes = require('../helpers/errorCodes');
+const adminOnly = require('../middlewares/adminOnly');
+const mongoose = require('mongoose');
 
 // Models
-const { CourseModel } = require("../models/Courses");
-const { SectionModel } = require("../models/Sections");
-const { ComponentModel } = require("../models/Components");
-const { UserModel } = require("../models/Users");
+const { CourseModel } = require('../models/Courses');
+const { SectionModel } = require('../models/Sections');
+const { ComponentModel } = require('../models/Components');
+const { UserModel } = require('../models/Users');
 const {
   ContentCreatorApplication,
-} = require("../models/ContentCreatorApplication");
-const requireLogin = require("../middlewares/requireLogin");
-const { IdentityStore } = require("aws-sdk");
+} = require('../models/ContentCreatorApplication');
+const requireLogin = require('../middlewares/requireLogin');
+const { IdentityStore } = require('aws-sdk');
 
 // Get all sections
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const list = await SectionModel.find();
   res.send(list);
 });
 
 // Get specific section
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const section = await SectionModel.findById(id);
   res.send(section);
