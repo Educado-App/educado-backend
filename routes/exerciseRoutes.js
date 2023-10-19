191ac5e6cc819cdc6e012e539d519edbe0f96bbd
Mafusn <Magnus.peetz.holt@gmail.com>
2023-10-19 12:26:13 +0200

    Clean up of imports

--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -1,19 +1,7 @@
 const router = require('express').Router();
-const errorCodes = require('../helpers/errorCodes');
-const adminOnly = require('../middlewares/adminOnly');
-const mongoose = require('mongoose');
 
 // Models
-const { CourseModel } = require('../models/Courses');
-const { SectionModel } = require('../models/Sections');
-const { ComponentModel } = require('../models/Components');
 const { ExerciseModel } = require('../models/Exercises');
-const { UserModel } = require('../models/Users');
-const {
-  ContentCreatorApplication,
-} = require('../models/ContentCreatorApplication');
-const requireLogin = require('../middlewares/requireLogin');
-const { IdentityStore } = require('aws-sdk');
 
 // Get all exercises
 router.get('/', async (req, res) => {
--- routes/sectionRoutes.js
+++ routes/sectionRoutes.js
@@ -1,18 +1,7 @@
 const router = require('express').Router();
-const errorCodes = require('../helpers/errorCodes');
-const adminOnly = require('../middlewares/adminOnly');
-const mongoose = require('mongoose');
 
 // Models
-const { CourseModel } = require('../models/Courses');
 const { SectionModel } = require('../models/Sections');
-const { ComponentModel } = require('../models/Components');
-const { UserModel } = require('../models/Users');
-const {
-  ContentCreatorApplication,
-} = require('../models/ContentCreatorApplication');
-const requireLogin = require('../middlewares/requireLogin');
-const { IdentityStore } = require('aws-sdk');
 
 // Get all sections
 router.get('/', async (req, res) => {
