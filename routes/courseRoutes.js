e44d199d9ddc201a78ffa2792d4fe5cfe6291fd3
Jatewo <jacobterpewoods@gmail.com>
2023-10-25 15:10:59 +0200

    feat(api): Added subscriber count functionality
    Changes made:
    - Added two error codes:
        -  Attempting to unsubscribe from a course you are not subscribed to
        - Attempting to subscribe to a course you are already subscribed to
    - Updated subscribe routes to send the updated user instead of the old one
    - Added an update to the coursemodels with an increment or decrement of one to the subscribe count for subscribe and unsubscribe respectively

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -9,7 +9,7 @@ const { ComponentModel } = require('../models/Components');
 const { ExerciseModel } = require('../models/Exercises');
 const { UserModel } = require('../models/Users');
 const {
-	ContentCreatorApplication,
+  ContentCreatorApplication,
 } = require("../models/ContentCreatorApplication");
 const requireLogin = require("../middlewares/requireLogin");
 const { IdentityStore } = require("aws-sdk");
@@ -20,116 +20,115 @@ const { IdentityStore } = require("aws-sdk");
 // Get all courses for one user
 router.get('/creator/:id', requireLogin, async (req, res) => {
   const id = req.params.id; // Get user id from request
-  const courses = await CourseModel.find({creator: id}); // Find courses for a specific user
-	
+  const courses = await CourseModel.find({ creator: id }); // Find courses for a specific user
+
   res.send(courses); // Send response
 });
 
 //Get all courses
 router.get('/', async (req, res) => {
 
-	try {
-		// find all courses in the database
-		const courses = await CourseModel.find();
+  try {
+    // find all courses in the database
+    const courses = await CourseModel.find();
 
-		// check if sections exist
-		if (courses.length === 0) {
-			// Handle "courses not found" error response here
-			return res.status(404).json({ 'error': errorCodes['E0005'] });
-		}
+    // check if sections exist
+    if (courses.length === 0) {
+      // Handle "courses not found" error response here
+      return res.status(404).json({ 'error': errorCodes['E0005'] });
+    }
 
-		res.send(courses);
-	} catch (error) {
-		// If the server could not be reached, return an error message
-		console.log(error);
-		return res.status(500).json({ 'error': errorCodes['E0003'] });
-	}
+    res.send(courses);
+  } catch (error) {
+    // If the server could not be reached, return an error message
+    console.log(error);
+    return res.status(500).json({ 'error': errorCodes['E0003'] });
+  }
 });
-
 
 // Get specific course
 router.get('/:id', async (req, res) => {
-	try {
-		const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-		// find a course based on it's id
-		const course = await CourseModel.findById(id);
+    // find a course based on it's id
+    const course = await CourseModel.findById(id);
 
-		// check if courses exist
-		if (!course) {
-			// Handle "course not found" error response here
-			return res.status(404).json({ 'error': errorCodes['E0006'] });
-		}
-		res.send(course);
-	} catch (error) {
-		return res.status(500).json({ 'error': errorCodes['E0003'] });
-	}
+    // check if courses exist
+    if (!course) {
+      // Handle "course not found" error response here
+      return res.status(404).json({ 'error': errorCodes['E0006'] });
+    }
+    res.send(course);
+  } catch (error) {
+    return res.status(500).json({ 'error': errorCodes['E0003'] });
+  }
 });
 
 // Get all sections from course
 router.get('/:id/sections', async (req, res) => {
 
-	try {
-		const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-		// find a course based on it's id
-		const course = await CourseModel.findById(id);
+    // find a course based on it's id
+    const course = await CourseModel.findById(id);
 
-		// check if courses exist
-		if (!course) {
-			// Handle "course not found" error response here
-			return res.status(404).json({ 'error': errorCodes['E0006'] });
-		}
+    // check if courses exist
+    if (!course) {
+      // Handle "course not found" error response here
+      return res.status(404).json({ 'error': errorCodes['E0006'] });
+    }
 
-		const sectionsInCourse = course.sections;
+    const sectionsInCourse = course.sections;
 
-		// check if course contains sections
-		if (sectionsInCourse.length === 0) {
-			// Handle "course does not contain sections" error response here
-			return res.status(404).json({ 'error': errorCodes['E0009'] });
-		}
+    // check if course contains sections
+    if (sectionsInCourse.length === 0) {
+      // Handle "course does not contain sections" error response here
+      return res.status(404).json({ 'error': errorCodes['E0009'] });
+    }
 
-		// check if the sections exists
-		const sectionsList = await SectionModel.find({ '_id': { $in: sectionsInCourse } });
-		if (sectionsList.length === 0) {
-			// Handle "course does not contain sections" error response here
-			return res.status(404).json({ 'error': errorCodes['E0007'] });
-		}
+    // check if the sections exists
+    const sectionsList = await SectionModel.find({ '_id': { $in: sectionsInCourse } });
+    if (sectionsList.length === 0) {
+      // Handle "course does not contain sections" error response here
+      return res.status(404).json({ 'error': errorCodes['E0007'] });
+    }
 
-		res.send(sectionsList);
+    res.send(sectionsList);
 
-	} catch (error) {
-		return res.status(500).json({ 'error': errorCodes['E0003'] });
-	}
+  } catch (error) {
+    return res.status(500).json({ 'error': errorCodes['E0003'] });
+  }
 
 });
 
 // Get a specififc section 
 router.get('/:courseId/sections/:sectionId', async (req, res) => {
 
-	try {
-		const { courseId, sectionId } = req.params;
+  try {
+    const { courseId, sectionId } = req.params;
 
-		const course = await CourseModel.findById(courseId);
-		// check if courses exist
-		if (!course) {
-			// Handle "course not found" error response here
-			return res.status(404).json({ 'error': errorCodes['E0006'] });
-		}
-		// find a specific section within the given course by both IDs
-		const section = await SectionModel.findOne({ parentCourse: courseId, _id: sectionId });
+    const course = await CourseModel.findById(courseId);
+    // check if courses exist
+    if (!course) {
+      // Handle "course not found" error response here
+      return res.status(404).json({ 'error': errorCodes['E0006'] });
+    }
+    // find a specific section within the given course by both IDs
+    const section = await SectionModel.findOne({ parentCourse: courseId, _id: sectionId });
 
-		// check if section exist
-		if (!section) {
-			// Handle "section not found" error response here
-			return res.status(404).json({ 'error': errorCodes['E0008'] });
-		}
+    // check if section exist
+    if (!section) {
+      // Handle "section not found" error response here
+      return res.status(404).json({ 'error': errorCodes['E0008'] });
+    }
 
-		res.send(section);
+    res.send(section);
 
-	} catch (error) {
-		return res.status(500).json({ 'error': errorCodes['E0003'] });
-	}
+  } catch (error) {
+    return res.status(500).json({ 'error': errorCodes['E0003'] });
+  }
 });
 
 /*** SUBSCRIPTION ROUTES ***/
@@ -137,80 +136,107 @@ router.get('/:courseId/sections/:sectionId', async (req, res) => {
 // Subscribe to course 
 router.post('/:id/subscribe', async (req, res) => {
 
-	try {
-		const { id } = req.params;
-		const { user_id } = req.body;
+  try {
+    const { id } = req.params;
+    const { user_id } = req.body;
 
-		const user = await UserModel.findById(user_id);
+    const user = await UserModel.findById(user_id);
 
-		//checks if user exist
-		if (!user) {
-			// Handle "user not found" error response here
-			return res.status(404).json({ 'error': errorCodes['E0004'] });
-		}
+    //checks if user exist
+    if (!user) {
+      // Handle "user not found" error response here
+      return res.status(404).json({ 'error': errorCodes['E0004'] });
+    }
 
-		const course = await CourseModel.findById(id);
-		// check if courses exist
-		if (!course) {
-			// Handle "course not found" error response here
-			return res.status(404).json({ 'error': errorCodes['E0006'] });
-		}
+    const course = await CourseModel.findById(id);
+    // check if courses exist
+    if (!course) {
+      // Handle "course not found" error response here
+      return res.status(404).json({ 'error': errorCodes['E0006'] });
+    }
 
-		// find user based on id, and add the course's id to the user's subscriptions field
-		(await UserModel.findOneAndUpdate(
-			{ _id: user_id },
-			{ $push: { subscriptions: id } }))
-			.save;
+    if (user.subscriptions.includes(id)) {
+      return res.status(400).json({ 'error': errorCodes['E0605'] }); //TODO: change error code
+    }
 
-		res.send(user);
+    course.numOfSubscriptions++;
+    user.subscriptions.push(id);
 
-	} catch (error) {
-		return res.status(500).json({ 'error': errorCodes['E0003'] });
-	}
+
+    // find user based on id, and add the course's id to the user's subscriptions field
+    await UserModel.findOneAndUpdate(
+      { _id: user_id },
+      { subscriptions: user.subscriptions });
+
+    await CourseModel.findOneAndUpdate(
+      { _id: course._id },
+      { numOfSubscriptions: course.numOfSubscriptions }
+    );
+
+
+    res.status(200).send(user);
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ 'error': errorCodes['E0003'] });
+  }
 
 });
 
 // Unsubscribe to course
 router.post('/:id/unsubscribe', async (req, res) => {
 
-	try {
-		const { id } = req.params;
-		const { user_id } = req.body;
+  try {
+    const { id } = req.params;
+    const { user_id } = req.body;
 
-		const user = await UserModel.findById(user_id);
-		//checks if user exist
-		if (!user) {
-			// Handle "user not found" error response here
-			return res.status(404).json({ 'error': errorCodes['E0004'] });
-		}
+    const user = await UserModel.findById(user_id);
+    //checks if user exist
+    if (!user) {
+      // Handle "user not found" error response here
+      return res.status(404).json({ 'error': errorCodes['E0004'] });
+    }
 
-		const course = await CourseModel.findById(id);
-		// check if courses exist
-		if (!course) {
-			// Handle "course not found" error response here
-			return res.status(404).json({ 'error': errorCodes['E0006'] });
-		}
+    const course = await CourseModel.findById(id);
+    // check if courses exist
+    if (!course) {
+      // Handle "course not found" error response here
+      return res.status(404).json({ 'error': errorCodes['E0006'] });
+    }
 
-		// find user based on id, and remove the course's id from the user's subscriptions field
-		(await UserModel.findOneAndUpdate(
-			{ _id: user_id },
-			{ $pull: { subscriptions: id } }))
-			.save;
+    if(!user.subscriptions.includes(id)){
+      return res.status(400).json({ 'error': errorCodes['E0606'] }); //TODO: change error code
+    }
 
-		res.send(user)
+    course.numOfSubscriptions--;
+    // Remove course id from user's subscriptions
+    user.subscriptions.indexOf(id) > -1 && user.subscriptions.splice(user.subscriptions.indexOf(id), 1);
 
-	} catch (error) {
-		return res.status(500).json({ 'error': errorCodes['E0003'] });
-	}
+    // find user based on id, and remove the course's id from the user's subscriptions field
+    await UserModel.findOneAndUpdate(
+      { _id: user_id },
+      { subscriptions: user.subscriptions })
+
+    await CourseModel.findOneAndUpdate(
+      { _id: course._id },
+      { numOfSubscriptions: course.numOfSubscriptions }
+    );
+
+    res.send(user)
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ 'error': errorCodes['E0003'] });
+  }
 
 });
 
 // Get all exercises for section
 router.get('/:section_id/exercises', async (req, res) => {
-	const { section_id } = req.params;
-	const section = await SectionModel.findById(section_id);
-	const list = await ExerciseModel.find({ _id: section.exercises });
-	res.send(list);
+  const { section_id } = req.params;
+  const section = await SectionModel.findById(section_id);
+  const list = await ExerciseModel.find({ _id: section.exercises });
+  res.send(list);
 });
 
 module.exports = router;
