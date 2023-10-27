66a4d3e89f27f07a0c862f14b2be61b1cce5cf36
MortenMunk <mortenmunk97@gmail.com>
2023-10-27 16:34:55 +0200

    fix(course tests) refactored tests
    - removed one test that only tested response, and not actual database object
    - added tests for unsubscribing, and subscribing that tests both numOfSubscriptions for courses, and subscriptions for users
    - added test for unsubscribing to a test without any subscribers
    - added numOfSubscriptions: 0 as fakeCourse object did not specify this amount

--- __tests__/fixtures/fakeCourse.js
+++ __tests__/fixtures/fakeCourse.js
@@ -1,13 +1,14 @@
 module.exports = function makeFakeCourse() {
 
-	return {
-		sections: [],
-		title: 'test course',
-		category: 'test',
-		difficulty: 1,
-		hours: 10,
-		description: "This course is a test course",
-		dateCreated: new Date(),
-		dateUpdated: new Date()
-	};
+  return {
+    sections: [],
+    title: 'test course',
+    category: 'test',
+    difficulty: 1,
+    hours: 10,
+    description: "This course is a test course",
+    dateCreated: new Date(),
+    dateUpdated: new Date(),
+    numOfSubscriptions: 0,
+  };
 };
