4ab60ca9d943d11dc210b4faa78a8803b7f87a7e
Frederik Bode <thorbensen@gmail.com>
2023-11-05 11:43:19 +0100

    Update cors.js
    Fixed cors issues

--- settings/cors.js
+++ settings/cors.js
@@ -1,16 +1,16 @@
 const cors = require("cors");
 
 const corsConfig = {
-	origin: [
-		"http://127.0.0.1:5173/",
-		"http://localhost:5173/",
-		"http://deft-bubblegum-6da164.netlify.app/",
-		"https://deft-bubblegum-6da164.netlify.app/",
-		"http://app-staging.educado.io/",
-		"https://app-staging.educado.io/",
-	],
-	optionsSuccessStatus: 200,
-	exposedHeaders: ["Content-Range", "X-Content-Range"],
+  origin: [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://deft-bubblegum-6da164.netlify.app",
+    "https://deft-bubblegum-6da164.netlify.app",
+    "http://app-staging.educado.io",
+    "https://app-staging.educado.io",
+  ],
+  optionsSuccessStatus: 200,
+  exposedHeaders: ["Content-Range", "X-Content-Range"],
 };
 
 // Export cors with config
