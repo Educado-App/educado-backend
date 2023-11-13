9ab0eb9a4e1da84f0c3cb56536b2916054af370b
Profft <stefanlarsen@live.dk>
2023-11-13 10:54:43 +0100

    A test

---
+++ __tests__/routes/utilityRoutes.test.js
@@ -0,0 +1,50 @@
+const request = require('supertest');
+const express = require('express');
+const router = require('../../routes/exerciseRoutes'); // Import your router file here
+const connectDb = require('../fixtures/db');
+const makeFakeUser = require('../fixtures/fakeUser');
+const makeFakeSection = require('../fixtures/fakeSection');
+const makeFakeExercise = require('../fixtures/fakeExercise');
+const mongoose = require('mongoose');
+const { signAccessToken } = require('../../helpers/token');
+const errorCodes = require('../../helpers/errorCodes')
+
+const app = express();
+app.use(express.json());
+app.use('/api/utility', router); // Add your router to the Express app
+
+// Mock Google OAuth2 clientID
+jest.mock('../../config/keys', () => {
+    return {
+        GOOGLE_CLIENT_ID: 'test',
+        TOKEN_SECRET: 'test',
+    };
+});
+
+// Start the Express app on a specific port for testing
+const PORT = 5021; // Choose a port for testing
+const server = app.listen(PORT)
+
+let fakeUser = makeFakeUser();
+let fakeSection = makeFakeSection();
+let fakeExercise = makeFakeExercise();
+
+
+describe('Test online response', () => {
+
+    it('Creates a exercise for a given course', async () => {
+        const token = signAccessToken({id: fakeUser._id});
+        const response = request(`http://localhost:${PORT}`)
+            .get('/api/utility/online/')
+            .expect(200);
+
+        expect(response.body).toBe(true);
+
+    });
+
+    afterAll(async () => {
+        server.close();
+        await mongoose.connection.close();
+    });
+
+});
