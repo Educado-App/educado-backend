be44eaee5b4ba59f9f824a8d54e82ce376c59896
Gogo <gogoskole@gmail.com>
2023-11-30 16:09:53 +0100

    Create exercise can now create lectures of type video. (Though video upload not fully implemented)

--- routes/lectureRoutes.js
+++ routes/lectureRoutes.js
@@ -42,13 +42,19 @@ router.get("/:id", async (req, res) => {
 router.put("/:section_id", /*requireLogin,*/ async (req, res) => {
   const {title, description, contentType} = req.body; //Handles the data in "data" from the request
   const section_id = req.params.section_id; //Handles the data in "params" from the request
+  let {content} = req.body;
+
+  //Video upload not fully implmented yet, therefore if its video we set it to empty string
+  if (content === null || content === undefined){
+    content = "";
+  }
 
   const lecture = new LectureModel ({
     parentSection: section_id,
     title: title,
     description: description,
     contentType: contentType,
-    content: "",
+    content: content,
     dateCreated: Date.now(),
     dateUpdated: Date.now()
   });
@@ -86,6 +92,7 @@ router.patch("/:id", /*requireLogin,*/ async (req, res) => {
     {
       title: lecture.title,
       description: lecture.description,
+      content: lecture.content,
       dateUpdated: Date.now(),
     },
     function (err, docs) {
