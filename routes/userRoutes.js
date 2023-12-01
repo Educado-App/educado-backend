df403b703c6c209873ccd6704f497aa26611f68b
Elias1998 <ehajji18@student.aau.dk>
2023-12-01 05:17:56 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -195,26 +195,26 @@ router.delete('/delete-experience/:_id', async (req, res) => {
 
 
 router.delete('/:id', requireLogin, async (req, res) => {
-  try {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return res.status(400).send({ error: errorCodes['E0014'] });
-    }
-    const id = mongoose.Types.ObjectId(req.params.id);
+	try {
+		if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+			return res.status(400).send({ error: errorCodes['E0014'] });
+		}
+		const id = mongoose.Types.ObjectId(req.params.id);
 
-    const deletedUser = await UserModel.findByIdAndDelete(id);
+		const deletedUser = await UserModel.findByIdAndDelete(id);
 
-    if (!deletedUser) {
-      return res.status(204).send(); // User not found
-    }
+		if (!deletedUser) {
+			return res.status(204).send(); // User not found
+		}
 
-    const deletedStudentProfile = await StudentModel.findOneAndDelete({ baseUser: id });
-    const deletedContentCreatorProfile = await ContentCreatorModel.findOneAndDelete({ baseUser: id });
+		const deletedStudentProfile = await StudentModel.findOneAndDelete({ baseUser: id });
+		const deletedContentCreatorProfile = await ContentCreatorModel.findOneAndDelete({ baseUser: id });
 
-    return res.status(200).send({
-      baseUser: deletedUser,
-      studentProfile: deletedStudentProfile,
-      contentCreatorProfile: deletedContentCreatorProfile
-    });
+		return res.status(200).send({
+			baseUser: deletedUser,
+			studentProfile: deletedStudentProfile,
+			contentCreatorProfile: deletedContentCreatorProfile
+		});
 
 
 	} catch (error) {
@@ -242,114 +242,114 @@ router.get('/:id', requireLogin, async (req, res) => {
 
 // Update User with dynamic fields
 router.patch('/:id', requireLogin, async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updateFields = req.body; // Fields to be updated dynamically
+	try {
+		const { id } = req.params;
+		const updateFields = req.body; // Fields to be updated dynamically
 
-    if (updateFields.password) {
-      return res.status(400).send({ error: errorCodes['E0803']})
-    }
+		if (updateFields.password) {
+			return res.status(400).send({ error: errorCodes['E0803'] });
+		}
 
-    const validFields = await validateFields(updateFields);
+		const validFields = await validateFields(updateFields);
 
-    const user = await UserModel.findById(id);
+		const user = await UserModel.findById(id);
 
-    if (!user) {
-      throw errorCodes['E0004']; // User not found
-    }
+		if (!user) {
+			throw errorCodes['E0004']; // User not found
+		}
 
-    if (!ensureNewValues(updateFields, user)) {
-      return res.status(400).send({ error: errorCodes['E0802'] })
-    }
+		if (!ensureNewValues(updateFields, user)) {
+			return res.status(400).send({ error: errorCodes['E0802'] });
+		}
 
-    
 
-    if (validFields) {
-      // Extracts the points and level fields from updateFields
 
-      const updatedUser = await UserModel.findByIdAndUpdate(
-        id,
-        { $set: updateFields, dateUpdated: Date.now() },
-        { new: true } // This ensures that the updated user document is returned
-      );
+		if (validFields) {
+			// Extracts the points and level fields from updateFields
 
-      res.status(200).send(updatedUser);
-    }
+			const updatedUser = await UserModel.findByIdAndUpdate(
+				id,
+				{ $set: updateFields, dateUpdated: Date.now() },
+				{ new: true } // This ensures that the updated user document is returned
+			);
 
-  } catch (error) {
-    if (error === errorCodes['E0004']) { // User not found
-      // Handle 'user not found' error response here
-      res.status(404).send({ error: errorCodes['E0004'] });
-    } else {
-      res.status(400).send({ error: error });
-    }
-  }
+			res.status(200).send(updatedUser);
+		}
+
+	} catch (error) {
+		if (error === errorCodes['E0004']) { // User not found
+			// Handle "user not found" error response here
+			res.status(404).send({ error: errorCodes['E0004'] });
+		} else {
+			res.status(400).send({ error: error });
+		}
+	}
 });
 
 // Update User password
 router.patch('/:id/password', requireLogin, async (req, res) => {
-  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.status(400).send({ error: errorCodes['E0014'] });
-  }
+	if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+		return res.status(400).send({ error: errorCodes['E0014'] });
+	}
 
-  const id = mongoose.Types.ObjectId(req.params.id);
-  const { oldPassword, newPassword } = req.body;
+	const id = mongoose.Types.ObjectId(req.params.id);
+	const { oldPassword, newPassword } = req.body;
 
-  if(!oldPassword || !newPassword) {
-    return res.status(400).send({ error: errorCodes['E0805'] });
-  }
+	if (!oldPassword || !newPassword) {
+		return res.status(400).send({ error: errorCodes['E0805'] });
+	}
 
-  const user = await UserModel.findById(id);
+	const user = await UserModel.findById(id);
 
-  if(!user) {
-    return res.status(400).send({ error: errorCodes['E0004'] });
-  }
+	if (!user) {
+		return res.status(400).send({ error: errorCodes['E0004'] });
+	}
 
-  if(!compare(oldPassword, user.password)) {
-    return res.status(400).send({ error: errorCodes['E0806'] });
-  }
+	if (!compare(oldPassword, user.password)) {
+		return res.status(400).send({ error: errorCodes['E0806'] });
+	}
 
-  try {
-    validatePassword(newPassword);
-  } catch (err) {
-    return res.status(400).send({ error: err });
-  }
+	try {
+		validatePassword(newPassword);
+	} catch (err) {
+		return res.status(400).send({ error: err });
+	}
 
-  user.password = encrypt(newPassword);
-  user.dateUpdated = Date.now();
+	user.password = encrypt(newPassword);
+	user.dateUpdated = Date.now();
 
-  const resUser = await UserModel.findByIdAndUpdate(id, user, { new: true });
+	const resUser = await UserModel.findByIdAndUpdate(id, user, { new: true });
 
-  resUser.password = undefined;
+	resUser.password = undefined;
 
-  return res.status(200).send(user);
+	return res.status(200).send(user);
 });
 
 /**
  * Validates the fields to be updated dynamically
  */
 async function validateFields(fields) {
-  const fieldEntries = Object.entries(fields);
+	const fieldEntries = Object.entries(fields);
 
-  for (const [fieldName, fieldValue] of fieldEntries) {
-    switch (fieldName) {
-      case 'email':
-        if (!(await validateEmail(fieldValue))) {
-          return false;
-        }
-        break;
-      case 'firstName':
-      case 'lastName':
-        if (!validateName(fieldValue)) {
-          return false;
-        }
-        break;
-      // Add more cases for other fields if needed
-      default:
-        throw errorCodes['E0801'];
-    }
-  }
-  return true;
+	for (const [fieldName, fieldValue] of fieldEntries) {
+		switch (fieldName) {
+		case 'email':
+			if (!(await validateEmail(fieldValue))) {
+				return false;
+			}
+			break;
+		case 'firstName':
+		case 'lastName':
+			if (!validateName(fieldValue)) {
+				return false;
+			}
+			break;
+			// Add more cases for other fields if needed
+		default:
+			throw errorCodes['E0801'];
+		}
+	}
+	return true;
 }
 
 module.exports = router;
