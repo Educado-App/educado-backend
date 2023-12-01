9ddd1c83a050db3c0ab8b35997b8aad5797d8de7
Elias1998 <ehajji18@student.aau.dk>
2023-12-01 05:10:40 +0100

    Update bucketRoutesGCP.js

--- routes/bucketRoutesGCP.js
+++ routes/bucketRoutesGCP.js
@@ -1,232 +1,155 @@
-const router = require("express").Router();
-const multer = require("multer");
-const axios = require("axios");
+const router = require('express').Router();
+const multer = require('multer');
+const axios = require('axios');
 const FormData = require('form-data');
 const { PassThrough } = require('stream');
 
-const { Storage } = require("@google-cloud/storage");
-const dotenv = require("dotenv");
-
 //Get serviceUrl from environment variable
+
+/* global process */
 const serviceUrl = process.env.TRANSCODER_SERVICE_URL;
 //const serviceUrl = "http://localhost:8080/api/v1";
-
-dotenv.config({ path: "./config/.env" });
-
-/* const credentials = process.env.GOOGLE_APPLICATION_CREDENTIALS;
- */const credentials = './config/gcp_service.json'
-// Creates a client
-
-// New GCP Bucket Instance
-const cloudStorage = new Storage({
-  projectId: 'credentials.project_id',
-  keyFilename: credentials,
-});
-
-// Constant variables
-const bucketName = "educado-bucket";
-const dir = "./_temp_bucketFiles";
-
-
-const uploadImg = multer({
-  storage: multer.memoryStorage(),
-  limits: {
-    fileSize: 5 * 1024 * 1024, // no larger than 5mb
-  },
-});
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-
 // Get list of all files in bucket
-router.get("/", (req, res) => {
-  //Forward to service api
-  axios.get(serviceUrl + "/bucket/").then((response) => {
-    res.send(response.data);
-  }).catch((error) => {
-    if (error.response && error.response.data) {
-      // Forward the status code from the Axios error if available
-      res.status(error.response.status || 500).send(error.response.data);
-    } else {
-      // Handle cases where the error does not have a response part (like network errors)
-      res.status(500).send({ message: "An error occurred during fecthing." });
-    }
-  });
+router.get('/', (req, res) => {
+	//Forward to service api
+	axios.get(serviceUrl + '/bucket/').then((response) => {
+		res.send(response.data);
+	}).catch((error) => {
+		if (error.response && error.response.data) {
+			// Forward the status code from the Axios error if available
+			res.status(error.response.status || 500).send(error.response.data);
+		} else {
+			// Handle cases where the error does not have a response part (like network errors)
+			res.status(500).send({ message: 'An error occurred during fecthing.' });
+		}
+	});
 });
 
 // Get file from bucket
-router.get("/:filename", (req, res) => {
-  //Forward to service api
-  axios.get(serviceUrl + "/bucket/" + req.params.filename).then((response) => {
-    res.send(response.data);
-  }).catch((error) => {
-    if (error.response && error.response.data) {
-      // Forward the status code from the Axios error if available
-      res.status(error.response.status || 500).send(error.response.data);
-    } else {
-      // Handle cases where the error does not have a response part (like network errors)
-      res.status(500).send({ message: "An error occurred during fetching." });
-    }
-  });
+router.get('/:filename', (req, res) => {
+	//Forward to service api
+	axios.get(serviceUrl + '/bucket/' + req.params.filename).then((response) => {
+		res.send(response.data);
+	}).catch((error) => {
+		if (error.response && error.response.data) {
+			// Forward the status code from the Axios error if available
+			res.status(error.response.status || 500).send(error.response.data);
+		} else {
+			// Handle cases where the error does not have a response part (like network errors)
+			res.status(500).send({ message: 'An error occurred during fetching.' });
+		}
+	});
 });
 
 // Delete file from bucket
-router.delete("/:filename", (req, res) => {
-  //Forward to service api
-  axios.delete(serviceUrl + "/bucket/" + req.params.filename).then((response) => {
-    res.send(response.data);
-  }).catch((error) => {
-    if (error.response && error.response.data) {
-      // Forward the status code from the Axios error if available
-      res.status(error.response.status || 500).send(error.response.data);
-    } else {
-      // Handle cases where the error does not have a response part (like network errors)
-      res.status(500).send({ message: "An error occurred during deletion." });
-    }
-  });
+router.delete('/:filename', (req, res) => {
+	//Forward to service api
+	axios.delete(serviceUrl + '/bucket/' + req.params.filename).then((response) => {
+		res.send(response.data);
+	}).catch((error) => {
+		if (error.response && error.response.data) {
+			// Forward the status code from the Axios error if available
+			res.status(error.response.status || 500).send(error.response.data);
+		} else {
+			// Handle cases where the error does not have a response part (like network errors)
+			res.status(500).send({ message: 'An error occurred during deletion.' });
+		}
+	});
 });
 
-function sanitizeFileName(fileName) {
-  return fileName.replace(/\.\./g, '').replace(/[^a-zA-Z0-9_.-]/g, '_');
-}
-
-
-// Upload file to GCP bucket
-router.post("/upload", uploadImg.single("file"), async (req, res) => {
-    const multerFile = req.file;
-    const fileName = sanitizeFileName(req.body.fileName);
-    
-    if (!multerFile) {
-      res.status(400).send("No file uploaded.");
-      return;
-    }
-    
-    // Validate MIME type
-    const validImageMimeTypes = [
-      "image/jpeg", "image/png", "video/mp4", 
-    ];
-    if (!validImageMimeTypes.includes(multerFile.mimetype)) {
-      res.status(400).send("Invalid file type. Please upload an image.");
-      return;
-    }
-    
-    const buffer = req.file.buffer;
-    
-    //upload to bucket
-    uploadFile().catch(console.error);
-    
-    
-    async function uploadFile() {
-      try {
-        await cloudStorage
-        .bucket(bucketName)
-        .file(fileName)
-        .save(buffer, {
-          metadata: {
-            contentType: multerFile.mimetype,
-          },
-        });
-          console.log(req.file)
-        res.status(200).send(fileName);
-      } 
-      catch (err) {
-        console.error(err);
-        res.status(500).send(`Error: ${err.message}`);
-      }
-    }
-  });
-  
-
 // Upload file to bucket
-router.post("/", upload.single("file"), (req, res) => {
-  const form = new FormData();
+router.post('/', upload.single('file'), (req, res) => {
+	const form = new FormData();
 
-  // Add file and filename to form
-  form.append('file', req.file.buffer, {
-    filename: req.file.originalname,
-    contentType: req.file.mimetype
-  });
-  form.append('fileName', req.body.fileName);
+	// Add file and filename to form
+	form.append('file', req.file.buffer, {
+		filename: req.file.originalname,
+		contentType: req.file.mimetype
+	});
+	form.append('fileName', req.body.fileName);
 
-  // Forward to service api
-  axios.post(serviceUrl + '/bucket/', form, { headers: form.getHeaders() })
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(error => {
-      if (error.response && error.response.data) {
-        // Forward the status code from the Axios error if available
-        res.status(error.response.status || 500).send(error.response.data);
-      } else {
-        // Handle cases where the error does not have a response part (like network errors)
-        res.status(500).send({ message: "An error occurred during upload." });
-      }
-    });
+	// Forward to service api
+	axios.post(serviceUrl + '/bucket/', form, { headers: form.getHeaders() })
+		.then(response => {
+			res.send(response.data);
+		})
+		.catch(error => {
+			if (error.response && error.response.data) {
+				// Forward the status code from the Axios error if available
+				res.status(error.response.status || 500).send(error.response.data);
+			} else {
+				// Handle cases where the error does not have a response part (like network errors)
+				res.status(500).send({ message: 'An error occurred during upload.' });
+			}
+		});
 });
 
 
 
 axios.interceptors.response.use(response => {
-  // This will process successful responses (within 2xx status codes)
-  return response;
+	// This will process successful responses (within 2xx status codes)
+	return response;
 }, error => {
-  // Handle error responses
-  if (error.response && error.response.data && typeof error.response.data.on === 'function') {
-    return new Promise((resolve, reject) => {
-      let errorData = '';
+	// Handle error responses
+	if (error.response && error.response.data && typeof error.response.data.on === 'function') {
+		return new Promise((resolve, reject) => {
+			let errorData = '';
 
-      error.response.data.on('data', (chunk) => {
-        errorData += chunk;
-      });
+			error.response.data.on('data', (chunk) => {
+				errorData += chunk;
+			});
 
-      error.response.data.on('end', () => {
-        // Once the entire error message is read from the stream, update the error object
-        error.response.data = errorData;
-        reject(error);
-      });
-    });
-  } else {
-    // Pass the original error through if the response data is not a stream
-    return Promise.reject(error);
-  }
+			error.response.data.on('end', () => {
+				// Once the entire error message is read from the stream, update the error object
+				error.response.data = errorData;
+				reject(error);
+			});
+		});
+	} else {
+		// Pass the original error through if the response data is not a stream
+		return Promise.reject(error);
+	}
 });
 
 // Stream file from bucket
-router.get("/stream/:filename", async (req, res) => {
-  try {
-      // Forward to Go service stream handler
-      const streamUrl = serviceUrl + "/stream/" + req.params.filename;
+router.get('/stream/:filename', async (req, res) => {
+	try {
+		// Forward to Go service stream handler
+		const streamUrl = serviceUrl + '/stream/' + req.params.filename;
 
-      // Make a GET request to the Go service
-      const response = await axios({
-          method: 'get',
-          url: streamUrl,
-          responseType: 'stream'
-      });
+		// Make a GET request to the Go service
+		const response = await axios({
+			method: 'get',
+			url: streamUrl,
+			responseType: 'stream'
+		});
 
-      // Check for errors from the Go service
-      if (response.status !== 200) {
-          res.status(response.status).send(response.data);
-          return;
-      }
+		// Check for errors from the Go service
+		if (response.status !== 200) {
+			res.status(response.status).send(response.data);
+			return;
+		}
 
-      // Set the headers from the Go service response
-      res.set(response.headers);
+		// Set the headers from the Go service response
+		res.set(response.headers);
 
-      // Pipe the response stream back to the client
-      const passThrough = new PassThrough();
-      response.data.pipe(passThrough);
-      passThrough.pipe(res);
+		// Pipe the response stream back to the client
+		const passThrough = new PassThrough();
+		response.data.pipe(passThrough);
+		passThrough.pipe(res);
 
-      // Handle any errors during streaming
-      passThrough.on('error', (error) => {
-          res.status(500).send("Error during streaming");
-      });
-  } catch (error) {
-      // Use the error response data (if available) when sending the error to the client
-      res.status(500).send(error.response ? error.response.data : "Error in request to Go service");
-  }
+		// Handle any errors during streaming
+		passThrough.on('error', () => {
+			res.status(500).send('Error during streaming');
+		});
+	} catch (error) {
+		// Use the error response data (if available) when sending the error to the client
+		res.status(500).send(error.response ? error.response.data : 'Error in request to Go service');
+	}
 });
 
 
