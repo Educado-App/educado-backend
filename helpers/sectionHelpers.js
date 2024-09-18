c6ab62b6f972a9f08d85fe13c9ef8ef25c20bd2c
Magnus K. Lundgren <magnus@themstrupvej.dk>
2024-09-18 14:46:31 +0200

    removed console.log

--- helpers/sectionHelpers.js
+++ helpers/sectionHelpers.js
@@ -2,7 +2,6 @@ const { SectionModel } = require('../models/Sections');
 
 
 function getLatestComponentFromSection(section) {
-	console.log(section.components.length);
 	return section.components[section.components.length - 1];
 }
 
