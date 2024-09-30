ab4e62214655b6c84a3f85b0a0375a888cf94034
Thomass1412 <thomas.lunoe@gmail.com>
2024-09-30 14:56:50 +0200

    ffs

--- __tests__/fixtures/fakeEmailVerificationToken.js
+++ __tests__/fixtures/fakeEmailVerificationToken.js
@@ -1,6 +1,6 @@
 module.exports = function makeFakeEmailVerificationToken(token='1234') {
 	return {
-        userEmail: 'fake@gmail.com',
+		userEmail: 'fake@gmail.com',
 		token: token,
 		expiresAt: new Date() + 1000 * 60 * 60 * 24 * 7,
 	};
--- models/EmailVerificationToken.js
+++ models/EmailVerificationToken.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const emailVerificationTokenSchema = new Schema({
-    userEmail: {
-        type: String,
-        required: true
-    },
-    token: {
-        type: String,
-        required: true,
-    },
-    expiresAt: {
-        type: Date,
-        required: true,
-    },
+	userEmail: {
+		type: String,
+		required: true
+	},
+	token: {
+		type: String,
+		required: true,
+	},
+	expiresAt: {
+		type: Date,
+		required: true,
+	},
 });
 
 const EmailVerificationToken = mongoose.model('emailVerificationToken', emailVerificationTokenSchema);
