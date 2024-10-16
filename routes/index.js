0d03a0e8ee101b8f1572b937add96016c030c116
madsWesth <72575386+madsWesth@users.noreply.github.com>
2024-10-16 15:14:57 +0200

    add institution routes

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ const StudentRoutes = require('../routes/studentRoutes');
 const ComponentRoutes = require('../routes/componentRoutes');
 const ProfileRoutes = require('../routes/profileRoutes');
 const UserInfoRoutes = require('../routes/userInfoRoutes');
+const InstituitionRoutes = require('./institutions/institutionsRoutes');
 
 // Print all routes defined in app
 router.get('/api', (req, res) => {
@@ -36,6 +37,7 @@ router.use('/api/students', StudentRoutes);
 router.use('/api/components', ComponentRoutes);
 router.use('/api/profiles', ProfileRoutes);
 router.use('/api/user-info', UserInfoRoutes);
+router.use('/api/institutions', InstituitionRoutes);
 
 // Test route
 router.use('/api/test', TestRoutes);
