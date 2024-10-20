e023bc806f4cce456cee88d5635f6e8d3028ecf9
LazarShockX <donaldbr12@gmail.com>
2024-10-20 18:22:23 +0200

    Revert endpoint

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -8,36 +8,25 @@ const { InstitutionModel } = require('../models/Institutions');
 
 //Route for when getting all applications
 router.get('/', async (req, res) => {
-    try {
-        // Find all content creators
-        const contentCreators = await ContentCreatorModel.find();
-        // Create an array that includes only the baseUser field of the above objects
-        const baseUserArray = contentCreators.map(array => array.baseUser);
+	try {
+		//Find all content creators who are not approved and not rejected
+		const contentCreators = await ContentCreatorModel.find({ approved: false, rejected: false });
+		//To find all appropriate users, create an array that that includes only the baseUser field of the above objects
+		const baseUserArray = contentCreators.map(array => array.baseUser);
         
-        // Find all users specified by array above
-        const applicators = await UserModel.find({ _id: { $in: baseUserArray } });
+		//Find all users specified by array above
+		const applicators = await UserModel.find({ _id: { $in: baseUserArray } });
 
-        // Merge contentCreators data with applicators data
-        const mergedData = applicators.map(applicator => {
-            const contentCreator = contentCreators.find(cc => cc.baseUser.toString() === applicator._id.toString());
-            return {
-                ...applicator.toObject(),
-                approved: contentCreator ? contentCreator.approved : undefined,
-                rejected: contentCreator ? contentCreator.rejected : undefined,
-            };
-        });
-
-        // Send a success response, and the relevant data
-        res.send({
-            success: true,
-            status: 200,
-            data: mergedData,
-        });
+		//Send a success response, and the relevant data
+		res.send({
+			success: true,
+			status: 200,
+			data: applicators});
             
-    } catch (error) {
-        // If anything unexpected happens, throw error
-        return res.status(404).json({ 'error': errorCodes['E0004'] }); // User not found
-    }
+	} catch (error) {
+		//If anything unexpected happens, throw error
+		return res.status(404).json({ 'error': errorCodes['E0004'] }); //User not found
+	}
 });
 
 //Route for getting specific content creator, and their application
