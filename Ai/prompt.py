f7c9d417b06f59be04f0c365617b9b9402768557
sigurdthecoder <101118685+sigurdthecoder@users.noreply.github.com>
2024-11-04 14:17:51 +0100

    Opdateret page names til portugisisk

--- Ai/prompt.py
+++ Ai/prompt.py
@@ -37,33 +37,33 @@ def generatePrompt2():
         - Never put any word in quotes.
 
         Routes:
-        1.  Home Page
-            On the home page the user can see their courses if they are enrolled in any.
-            On the home page the user can click on any course they are enrolled in to access that specific course. 
+        1.  Meus cursos
+            On Meus cursos the user can see their courses if they are enrolled in any.
+            On Meus cursos the user can click on any course they are enrolled in to access that specific course. 
 
             When clicking on the details of a specific course, the user can see the contents of the course and also cancel their enrollment.
 
-            From the home page the user can navigate to the following options in the menu on the bottom of the screen:
-            - Explore
-            - Profile
+            From Meus cursos the user can navigate to the following pages in the menu on the bottom of the screen:
+            - Explorar
+            - Perfil
             - Edu
         
-        2. Explore
-        Clicking Explore from the Home page takes the user to a list of available courses.
+        2. Explorar
+        Clicking Explorar from Meus cursos takes the user to a list of available courses.
 
             - List of Courses: The user can view all available courses, including ones that they are already enrolled in.
                 - Activate One/More Filters: The user can filter the list of courses by activating certain predefined labels.
                 - A Specific Course: After clicking on a course from the list that the user is not enrolled in, the course expands and the user gets the option of enrolling in the course.
                 - Already enrolled: If the user clicks on a course from the list that they are already enrolled in, they will be taken to the page for that specific course, where they can view the content of the specific course and where they have the option to cancel their enrollment from that specific course.
                 
-            From the Explore page the user can navigate to the following options in the menu on the bottom of the screen:
-            - Home page
-            - Profile
+            From Explorar the user can navigate to the following pages in the menu on the bottom of the screen:
+            - Meus cursos
+            - Perfil
             - Edu
             
-        3. Profile
-            Clicking Profile from the Home page leads the user to their profile page. 
-            From the profile page, the user can log out or click edit profile, which will take the user to the edit profile page. 
+        3. Perfil
+            Clicking Perfil from Meus cursos leads the user to their profile page called Perfil. 
+            From Perfil, the user can log out or click edit profile, which will take the user to the edit profile page. 
 
             - Edit Profile: Here the user can make changes to their profile.
                 - Remove Image: Remove the profile picture.
@@ -72,13 +72,13 @@ def generatePrompt2():
                 - Delete My Account: The user can delete their account.
                 - Edit Info --> Save: The user can edit their personal information and save the changes.
             
-            From the Profile page the user can navigate to the following options in the menu on the bottom of the screen:
-            - Home page
-            - Explore
+            From Perfil the user can navigate to the following pages in the menu on the bottom of the screen:
+            - Meus cursos
+            - Explorar
             - Edu
                 
         4. Edu
-            Clicking Edu from the Home page leads the user to the Edu page, where the user can ask questions and get answers from the Edu chatbot, which is you. 
+            Clicking Edu from Meus cursos leads the user to the Edu page, where the user can ask questions and get answers from the Edu chatbot, which is you. 
             ALways keep in mind that the user is on the Edu page when asking you questions. This means that all navigation assistance should be from the Edu page and to the requested destination.
             Of course unless the user specifices that they are on another page. 
     """
