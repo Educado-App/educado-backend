f4d66ca4ebce1c876ad8f1920a4142553a664f4b
Kasper Siewierkiewicz <117650279+Kasper-Sie@users.noreply.github.com>
2024-11-12 13:55:16 +0100

    changed error messages

--- middlewares/rate_limiting.js
+++ middlewares/rate_limiting.js
@@ -3,13 +3,13 @@ const rateLimit = require('express-rate-limit');
 const shorttermLimiter = rateLimit({
     windowMs: 10 * 1000, // 10 seconds for testing
     max: 5, // Limit each IP to 5 requests per 10 seconds
-    message: "Too many requests, please try again later, fuck off please, u idiot",
+    message: { error: "Slow down! Too many requests." },
 });
 
 const longtermLimiter = rateLimit({
     windowMs: 60 * 60 * 1000, 
     max: 100, // Limit each IP to 100 requests per hour
-    message: "Too many requests, long term failure",
+    message: { error: "You have exceeded the hourly limit. Please try again later." },
 });
 
 
