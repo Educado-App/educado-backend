28e4bdac618464c1b81ae1df6887faa57acf2653
Anders <andersraae01@gmail.com>
2024-11-21 10:34:52 +0100

    groupTree-114 db create creator certificate and get creator certificate done

--- routes/certificateRoutes.js
+++ routes/certificateRoutes.js
@@ -1,26 +1,16 @@
 const router = require("express").Router();
-const { ContentCreator } = require("../models/ContentCreators");
-const { Course } = require("../models/Courses");
+const { CourseModel } = require("../models/Courses");
 const {
   CertificateContentCreatorModel,
 } = require("../models/CertificateContentCreator");
 const errorCodes = require("../helpers/errorCodes");
 
-router.post("/creator-certificates", async (req, res) => {
+// Get creator certificate
+router.get("/get-creator-certificates/:creatorId", async (req, res) => {
   try {
-    const { creatorId, courseId } = req.body;
-    const certificate = await createCertificate(creatorId, courseId);
-    res.status(201).json(certificate);
-  } catch (error) {
-    res.status(500).json({ error: "Error creating certificate" });
-  }
-});
-
-router.get("/creator-certificates/:creatorId", async (req, res) => {
-  try {
-    const { creatorId } = req.params;
+    const creatorId = req.params.creatorId;
     const certificates = await CertificateContentCreatorModel.findOne({
-      _id: creatorId,
+      creator: creatorId,
     })
       .populate("creator-certificates")
       .exec();
@@ -30,6 +20,40 @@ router.get("/creator-certificates/:creatorId", async (req, res) => {
     console.error("Error fetching certificates:", error);
     res.status(500).json({ error: "Error fetching certificates" });
   }
+});
+
+// Create certificate route 
+router.put('/create-creator-certificate', async (req, res) => {
+	const { courseId, creatorId } = req.body;
+
+  // Time of creation
+
+  const course = await CourseModel.findOne({ _id: courseId, });
+
+  if (!course) {
+		return res.status(400).send({ error: errorCodes['E0006'] }); // If course does not exist, return error
+	}
+
+	const contentCreatorCertificate = new CertificateContentCreatorModel({
+		title: course.title,
+		category: course.category,
+		creator: creatorId,
+    rating: course.rating,
+    numOfSubscriptions: 0,
+		dateCreated: Date.now(),
+		dateUpdated: Date.now(),
+	});
+
+	try {
+		const result = contentCreatorCertificate.save({ new: true })
+			//As id is generated at save, we need to .then() and then save
+			.then(savedContentCreatorCertificate => {
+				return savedContentCreatorCertificate.save();
+			});
+		return res.status(201).send(result);
+	} catch (err) {
+		return res.status(400).send(err);
+	}
 });
 
 module.exports = router;
