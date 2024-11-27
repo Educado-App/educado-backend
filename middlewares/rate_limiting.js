4dc4880866bd7fdd722437d76531a0bf2fc5e748
Kasper Siewierkiewicz <117650279+Kasper-Sie@users.noreply.github.com>
2024-11-27 14:33:38 +0100

    changed some english to portugese

--- middlewares/rate_limiting.js
+++ middlewares/rate_limiting.js
@@ -3,13 +3,13 @@ const rateLimit = require('express-rate-limit');
 const shorttermLimiter = rateLimit({
 	windowMs: 10 * 1000, // 10 seconds for testing
 	max: 50000, // Limit each IP to 5 requests per 10 seconds
-	message: { error: 'Slow down! Too many requests.' },
+	message: { error: 'Desacelere! Muitas solicitações.' },
 });
 
 const longtermLimiter = rateLimit({
 	windowMs: 60 * 60 * 1000, 
 	max: 1000000, // Limit each IP to 100 requests per hour
-	message: { error: 'You have exceeded the hourly limit. Please try again later.' },
+	message: { error: 'Você excedeu o limite por hora. Por favor, tente novamente mais tarde.' },
 });
 
 
